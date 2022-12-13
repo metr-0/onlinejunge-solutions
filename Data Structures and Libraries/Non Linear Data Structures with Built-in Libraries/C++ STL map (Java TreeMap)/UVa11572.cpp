82540a946e951b51cd23e41a56ff25c76f9b41f7
metr-0 <mar2017ar@gmail.com>
2022-12-13 09:24:33 +0300

    add gitignore

--- Data Structures and Libraries/Non Linear Data Structures with Built-in Libraries/C++ STL map (Java TreeMap)/UVa11572.cpp
+++ Data Structures and Libraries/Non Linear Data Structures with Built-in Libraries/C++ STL map (Java TreeMap)/UVa11572.cpp
@@ -1,28 +1,29 @@
 #include <cstdio>
-#include <map>
+#include <bitset>
 
-using mii = std::map<int, int>;
+std::bitset<1000000005> f;
+int *snowflakes = new int[1000000005];
 
 void solve()
 {
 	int SC;
 	scanf("%d", &SC);
 	
-	mii snowflakes;
+	f.reset();
 	
 	int max = 0, start = 0, v, temp;
 	for (int i = 0; i < SC; i++) {
 		scanf("%d", &v);
 		
-		if (snowflakes.find(v) != snowflakes.end()) {
+		if (f[v]) {
 			temp = i - start;
 			
 			if (start < snowflakes[v] + 1)
 				start = snowflakes[v] + 1;
 			
-			snowflakes[v] = i;
-			
 			if (temp > max) max = temp;
+		} else {
+			f[v] = true;
 		}
 		
 		snowflakes[v] = i;
